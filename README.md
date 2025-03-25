3768b9c9c7fd25e5f3c6e14447ca9f1e36b33d95
Gobi Mahato <116241775+gobiweb@users.noreply.github.com>
2025-03-25 11:41:49 +0530

    initial setup (shadcn ui)

---
+++ README.md
@@ -0,0 +1,1 @@
+# Trimrr URL Shortener
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <h1 className="mb-4 text-5xl text-red-500">App</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
